f410249672c526b0fd435806ddebf00beb5b9823
bhnord <b.nordemann@aol.com>
2024-04-21 18:36:12 -0400

    fixed csv

--- dataset/convert.py
+++ dataset/convert.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 
-df = pd.read_csv("./GDP.csv")
+df = pd.read_csv("GDP.csv")
 ignore = ["Country Name","Country Code","Indicator Name","Indicator Code"]
 for index, row in df.iterrows():
     country_code = row[1]
 
-    with open(country_code+".csv", "w") as file:
+    with open("GDP/"+country_code+".csv", "w") as file:
+        file.write("date, val\n")
         for name, val in row.items():
             if name not in ignore:
                 file.write(str(name) + ", " + str(val) + "\n")
