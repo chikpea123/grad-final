bc40286aa251cda11998dbe8a3a2b343625f068a
bhnord <b.nordemann@aol.com>
2024-04-21 18:38:53 -0400

    csv2

--- dataset/convert.py
+++ dataset/convert.py
@@ -6,7 +6,7 @@ for index, row in df.iterrows():
     country_code = row[1]
 
     with open("GDP/"+country_code+".csv", "w") as file:
-        file.write("date, val\n")
+        file.write("date,val\n")
         for name, val in row.items():
             if name not in ignore:
                 file.write(str(name) + "," + str(val) + "\n")
