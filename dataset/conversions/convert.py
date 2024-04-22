3bf28d2cbbc0f6399a9220469fec36ac547bec9e
bhnord <b.nordemann@aol.com>
2024-04-22 12:53:19 -0400

    parsed csv values

--- dataset/conversions/convert.py
+++ dataset/conversions/convert.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("../GDP.csv")
+df = pd.read_csv("../Unemployment.csv")
 ignore = ["Country Name", "Country Code", "Indicator Name", "Indicator Code"]
 for index, row in df.iterrows():
     country_code = row[1]
 
-    with open("../GDP/" + country_code + ".csv", "w") as file:
+    with open("../Unemployment/" + country_code + ".csv", "w") as file:
         file.write("date,val\n")
         for name, val in row.items():
             if name not in ignore:
