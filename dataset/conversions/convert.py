b4e8971d12f714863efb8da508c4681d43a870ad
bhnord <b.nordemann@aol.com>
2024-04-22 11:41:41 -0400

    restructured helper files

--- dataset/conversions/convert.py
+++ dataset/conversions/convert.py
@@ -1,11 +1,11 @@
-import pandas as pd 
+import pandas as pd
 
-df = pd.read_csv("GDP.csv")
-ignore = ["Country Name","Country Code","Indicator Name","Indicator Code"]
+df = pd.read_csv("../GDP.csv")
+ignore = ["Country Name", "Country Code", "Indicator Name", "Indicator Code"]
 for index, row in df.iterrows():
     country_code = row[1]
 
-    with open("GDP/"+country_code+".csv", "w") as file:
+    with open("../GDP/" + country_code + ".csv", "w") as file:
         file.write("date,val\n")
         for name, val in row.items():
             if name not in ignore:
